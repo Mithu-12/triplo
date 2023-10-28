510b9e8ce6e7e1bd58431d3307afc449085110cc
mithu-12 <mithuvowmick96@gmail.com>
2023-10-28 10:06:43 +0600

    google update11

--- src/components/Loader/LoaderSpiner.jsx
+++ src/components/Loader/LoaderSpiner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import './Loader.css'
+import './LoaderSpiner.css'
 
 const LoaderSpiner = () => {
   return (
